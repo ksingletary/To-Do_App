61f39273bdf70292c25ec92a3b462f9f242723b8
Keith Kodes <code.keith@Keiths-MBP.lan>
2023-09-04 15:42:53 -0400

    Probly simpler solution. But looks ok nonetheless.

--- app.js
+++ app.js
@@ -1,18 +1,37 @@
 
 const button = document.querySelector('.button');
+const Todos = [];
 button.addEventListener('click', function(evt){
+
+    //setting variables/classes
+
     evt.preventDefault();
     var inputValue = document.getElementById("myInput").value;
     const newLi = document.createElement('li');
     newLi.setAttribute("class", "rmv");
     newLi.appendChild(document.createTextNode(inputValue));
     const ulLength = document.getElementById('myUL').getElementsByTagName('li').length;
+    const firstUL = document.getElementById('myUL').getElementsByTagName('li');
+
+    //if input is empty alert user, otherwise add it to list
+
     if (inputValue === '' && ulLength < 1) {
         alert("Please add a task to your list");
       } else if (inputValue === '' && ulLength >= 1){
         alert("Please enter a task");
       } else {
         document.getElementById("myUL").appendChild(newLi);
+        Todos.push({task: newLi.innerText, isCompleted: false });
+
+        //add to localstorage
+
+        function addToLocalStorage(todos) {
+            localStorage.setItem('todos', JSON.stringify(Todos));
+          }
+        addToLocalStorage(Todos);
+
+        //adding delete and complete buttons to list item
+
         let deleteBtn = document.createElement("button");
         let completeBtn = document.createElement("button");
         completeBtn.textContent = "Completed"
@@ -26,16 +45,66 @@ button.addEventListener('click', function(evt){
             newLi.remove();
         })  
         completeBtn.addEventListener('click', function(evt){
-            evt.preventDefault();
+            var node = document.querySelector("li");
             newLi.style.textDecoration = "line-through";
+            document.getElementById("compUL").appendChild(node);
         })   
       }
     document.getElementById("myInput").value = "";
     
 })
 
+//retrieve from local storage
 
+const displayList = items => {
+        items.forEach((todos) => {
+            const li = document.createElement("li");
+            li.innerHTML = todos.task;
+            let deleteBtn = document.createElement("button");
+            let completeBtn = document.createElement("button");
+            completeBtn.textContent = "Completed"
+            deleteBtn.innerText = "X"
+            deleteBtn.setAttribute("id", "delete-btn")
+            completeBtn.setAttribute("class", "complete");
+            li.append(completeBtn);
+            li.append(deleteBtn);
+            deleteBtn.addEventListener('click', function(evt){
+                evt.preventDefault();
+                li.remove();
+                localStorage.removeItem('todos');
+            })  
+            completeBtn.addEventListener('click', function(evt){
+                var node = document.querySelector("li");
+                li.style.textDecoration = "line-through";
+                document.getElementById("compUL").appendChild(node);
+            })   
+            
+            document.getElementById("myUL").appendChild(li);
+        });
+        };
+        if (JSON.parse(localStorage.getItem('todos')) !==""){
+            const name = JSON.parse(localStorage.getItem("todos")) || [];
+            displayList(name);
+        }   else{
+            localStorage.removeItem('todos')
+        }
 
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+if (localStorage.getItem('darkModeEnabled')){
+    document.body.className = 'dark';
+    toggleSwitch.checked = true;
+}
+
+toggleSwitch.addEventListener('click', function(evt){
+    const {checked} = toggleSwitch;
+    if(checked){
+        localStorage.setItem('darkModeEnabled', true)
+    } else{
+        localStorage.removeItem('darkModeEnabled');
+    }
+    document.body.className = checked ? 'dark' : ""
+})
 
 
 
